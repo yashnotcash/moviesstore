002d313fc8cd8983a3ccaf04bfe18f61a5505496
yashnotcash <ysinghal8@gatech.edu>
2025-05-28 19:47:40 -0400

    movies-model

--- moviesstore/movies/admin.py
+++ moviesstore/movies/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Movie
+class MovieAdmin(admin.ModelAdmin):
+    ordering = ['name']
+admin.site.register(Movie, MovieAdmin)
--- moviesstore/moviesstore/settings.py
+++ moviesstore/moviesstore/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [ BASE_DIR / 'moviesstore/static/', ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
