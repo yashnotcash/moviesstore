c81eda0694ac6c8e3f870224dfcdbb132f55a31a
yashnotcash <ysinghal8@gatech.edu>
2025-05-28 20:04:18 -0400

    added signups+login

--- moviesstore/moviesstore/urls.py
+++ moviesstore/moviesstore/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('movies/', include('movies.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
