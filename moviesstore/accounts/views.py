e7194f76bd818033c07c34857ed9602b8fbcce1b
Maks Kushniryk <maks@lawn-143-215-124-173.lawn.gatech.edu>
2025-05-30 20:25:37 -0400

    chapter 12 done

--- moviesstore/accounts/views.py
+++ moviesstore/accounts/views.py
@@ -3,6 +3,14 @@ from django.contrib.auth import login as auth_login, authenticate, logout as aut
 from .forms import CustomUserCreationForm, CustomErrorList
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+@login_required
+def orders(request):
+    template_data = {}
+    template_data['title'] = 'Orders'
+    template_data['orders'] = request.user.order_set.all()
+    return render(request, 'accounts/orders.html',
+        {'template_data': template_data})
 
 @login_required
 def logout(request):
--- moviesstore/moviesstore/urls.py
+++ moviesstore/moviesstore/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('movies/', include('movies.urls')),
     path('accounts/', include('accounts.urls')),
+    path('cart/', include('cart.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
